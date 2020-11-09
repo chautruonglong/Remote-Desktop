805a41db615be218239c8f70e5951b41dfe58286
mycodex <truonglongchau@gmail.com>
2020-11-09 13:11:08 +0700

    Update send file

--- src/com/gui/ChatPanel.java
+++ src/com/gui/ChatPanel.java
@@ -202,6 +202,7 @@ public class ChatPanel extends JPanel implements Runnable {
                         this.common_bus.getChatBus().sendMessage(file_message);
 
                         int gap = this.scroll_panel.getWidth() - 180;
+                        file_message.setSender("You send a file");
                         FileLabel file_label = new FileLabel(file_message);
                         this.addMessageToPanel(file_label, gap, "green");
 
